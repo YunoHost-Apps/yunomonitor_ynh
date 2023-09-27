4c67df134b3aacc9d8edfbe91fe0b9182de92d4a
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2023-09-27 11:16:17 +0200

    Add python3-dev dependency in attempt to fix build

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3-dbus python3-cryptography python3-paramiko python3-yaml python3-dnspython python3-requests-toolbelt python3-spf whois smartmontools"
+pkg_dependencies="python3-dbus python3-cryptography python3-paramiko python3-yaml python3-dnspython python3-requests-toolbelt python3-spf whois smartmontools python3-dev"
 
 #=================================================
 # PERSONAL HELPERS
